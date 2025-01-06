0c9235db44c927bf7c58de9f3414d8143360f35f
Jorge Chaux <chauxjorge@hotmail.com>
2025-01-06 09:39:59 -0500

    Add 01 no frills react PROPERTIES

--- 01-no frills-react/src/App.js
+++ 01-no frills-react/src/App.js
@@ -1,10 +1,10 @@
-const Pizza = () =>  {
+const Pizza = (props) =>  {
     return React.createElement(
         "div",
         {},
         [
-            React.createElement("h1", {}, "The Pepperoni Pizza"),
-            React.createElement("p", {}, "Mozzarella Cheese, Pepperoni"),          
+            React.createElement("h1", {}, props.name),
+            React.createElement("p", {}, props.description),          
             
         ]
     ); 
@@ -16,11 +16,26 @@ const App = () =>  {
         {},
         [
             React.createElement("h1", {}, "Padre Gino's"),
-            React.createElement(Pizza),
-            React.createElement(Pizza),
-            React.createElement(Pizza),
-            React.createElement(Pizza),
-            React.createElement(Pizza),
+            React.createElement(Pizza, {
+                name: "The pepperoni Pizza",
+                description: "some deli pizza!",
+            }),
+            React.createElement(Pizza, {
+                name: "American pizza",
+                description: "Frencj fries and hot dogs",
+            }),
+            React.createElement(Pizza, {
+                name: "The Hawaiian",
+                description: "Pineaple and Ham",
+            }),
+            React.createElement(Pizza, {
+                name: "The chiken pizza",
+                description: "Chick.",
+            }),
+            React.createElement(Pizza, {
+                name: "The Big Meat Pizza",
+                description: "Bacon, Pepperoni, Italian Sausage, Chorizo Sausage",
+            }),
         ]
         
     ); 
