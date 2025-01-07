9a56d488722cb9f413b347e7b704e9221a05e052
Jorge Chaux <chauxjorge@hotmail.com>
2025-01-07 15:47:25 -0500

    Add initial code for 03-jsx

---
+++ 03-jsx/src/App.js
@@ -0,0 +1,39 @@
+import React from "react";
+import { createRoot } from "react-dom/client";
+
+const Pizza = (props) => {
+  return React.createElement("div", {}, [
+    React.createElement("h1", {}, props.name),
+    React.createElement("p", {}, props.description),
+  ]);
+};
+
+const App = () => {
+  return React.createElement("div", {}, [
+    React.createElement("h1", {}, "Padre Gino's"),
+    React.createElement(Pizza, {
+      name: "The pepperoni Pizza",
+      description: "some deli pizza!",
+    }),
+    React.createElement(Pizza, {
+      name: "American pizza",
+      description: "Frencj fries and hot dogs",
+    }),
+    React.createElement(Pizza, {
+      name: "The Hawaiian",
+      description: "Pineaple and Ham",
+    }),
+    React.createElement(Pizza, {
+      name: "The chiken pizza",
+      description: "Chick.",
+    }),
+    React.createElement(Pizza, {
+      name: "The Big Meat Pizza",
+      description: "Bacon, Pepperoni, Italian Sausage, Chorizo Sausage",
+    }),
+  ]);
+};
+
+const container = document.getElementById("root");
+const root = createRoot(container);
+root.render(React.createElement(App));
