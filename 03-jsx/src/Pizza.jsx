1de8ec20188cb47361ff844499b26346e9014103
Jorge Chaux <chauxjorge@hotmail.com>
2025-01-08 10:38:58 -0500

    Add static assets

--- 03-jsx/src/Pizza.jsx
+++ 03-jsx/src/Pizza.jsx
@@ -1,20 +1,11 @@
-// const Pizza = (props) => {
-//   return React.createElement("div", {}, [
-//     React.createElement("h1", {}, props.name),
-//     React.createElement("p", {}, props.description),
-//   ]);
-// };
-
-//This is the markup you intend to generate
-//This is easiest way to read and is the same as the code above.
-
 const Pizza = (props) => {
-    return (
-      <div className="pizza">
-        <h1>{ props.name }</h1>
-        <p>{ props.description }</p>
-      </div>
-    );
-  };
+  return (
+    <div className="pizza">
+      <h1>{props.name}</h1>
+      <p>{props.description}</p>
+      <img src={props.image} alt={props.name} />
+    </div>
+  );
+};
 
 export default Pizza;
